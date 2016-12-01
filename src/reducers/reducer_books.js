bc5ae7b03fee340c67920ca571ba6d57f8b4fe73
sahilnayak <snayak.researcher@gmail.com>
2016-12-01 14:43:46 -0800

    Adding in the first app containers and reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript the Hard Parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ];
+}
