1e3c5285ef60139604fbdeed7b313fbcf7e090fd
sahilnayak <snayak.researcher@gmail.com>
2016-12-02 16:05:05 -0800

    Finished connecting components with the corresponding actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,11 @@
 export function selectBook(book) {
-    console.log('Book has been selected', book.title);
+    // selectBook is an ActionCreator, it needs to return an ActionCreator
+    // an object with  a type property
+    console.log(book.title);
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    };
+
 }
 
